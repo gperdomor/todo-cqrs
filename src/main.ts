4f01ce64e1b9a5594d887dc679fee43a9d46214a
Gustavo Perdomo <gperdomor@gmail.com>
2019-12-09 22:47:58 -0300

    feat: added swagger and other usefull config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
 import { AppModule } from './app.module';
+import { configure } from './configure';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const { hostname, port, prefix } = configure(app);
+
+  await app.listen(port, hostname, () => {
+    // tslint:disable-next-line: no-console
+    console.log(`Listening at http://${hostname}:${port}/${prefix}`);
+  });
 }
 bootstrap();
